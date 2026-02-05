1f3e8227da4019eb86bc4e3197f078d223986a66
moncef1818 <monomoncef7@gmail.com>
2026-02-05 20:02:37 +0100

    uu

--- src/assets/Register_page/Components/RegForm.jsx
+++ src/assets/Register_page/Components/RegForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-import './Reg_Form.css'
+import './Reg_form.css'
 
 const RegForm = () => {
   const [selectedPass, setSelectedPass] = useState(null);
